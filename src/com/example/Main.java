be71c74d3c2d8aa265502a96bc45d5d36b8df399
Ian Lee <ian102423@github.com>
2017-07-31 17:21:11 -0500

    Code Smell Changed

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -13,84 +13,44 @@ public class Main {
         try {
             Scanner scan1 = new Scanner(System.in);
             System.out.println("What month do you need?");
-            String chooseMonth = scan1.nextLine().toUpperCase();
+            String chosenMonth = scan1.nextLine().toUpperCase();
 
-            Month month = Month.valueOf(chooseMonth);
+            Month month = Month.valueOf(chosenMonth);
 
             switch (month) {
                 case JANUARY:
-                    System.out.println("January is: " + Month.JANUARY.getKoreanName() + "\n");
                 case FEBRUARY:
-                    System.out.println("February is: " + Month.FEBRUARY.getKoreanName() + "\n");
                 case MARCH:
-                    System.out.println("March is: " + Month.MARCH.getKoreanName() + "\n");
                 case APRIL:
-                    System.out.println("April is: " + Month.APRIL.getKoreanName() + "\n");
                 case MAY:
-                    System.out.println("May is: " + Month.MAY.getKoreanName() + "\n");
                 case JUNE:
-                    System.out.println("June is: " + Month.JUNE.getKoreanName() + "\n");
                 case JULY:
-                    System.out.println("July is: " + Month.JULY.getKoreanName() + "\n");
                 case AUGUST:
-                    System.out.println("August is: " + Month.AUGUST.getKoreanName() + "\n");
                 case SEPTEMBER:
-                    System.out.println("September is: " + Month.SEPTEMBER.getKoreanName() + "\n");
                 case OCTOBER:
-                    System.out.println("October is: " + Month.OCTOBER.getKoreanName() + "\n");
                 case NOVEMBER:
-                    System.out.println("November is: " + Month.NOVEMBER.getKoreanName() + "\n");
                 case DECEMBER:
-                    System.out.println("December is: " + Month.DECEMBER.getKoreanName() + "\n");
+                    System.out.printf("%s is: " + month.getKoreanName() + "\n", month);
                     break;
                 default:
                     System.out.println("Month is not existing\n");
 
             }
 
-            String addToMonthList = chooseMonth;
-
-            if (addToMonthList == "january" || addToMonthList == "february" || addToMonthList == "march" || addToMonthList == "april" || addToMonthList == "may" || addToMonthList == "june" || addToMonthList == "july" || addToMonthList == "august" || addToMonthList == "september" || addToMonthList == "october" || addToMonthList == "november" || addToMonthList == "december") {
-                addToMonthList = chooseMonth;
-            }
-
-
-            switch (addToMonthList) {
+            switch (chosenMonth.toLowerCase()) {
                 case "january":
-                    System.out.println("January is added to the Month list\n");
-                    break;
                 case "february":
-                    System.out.println("February is added to the Month list\n");
-                    break;
                 case "march":
-                    System.out.println("March is added to the Month list\n");
-                    break;
                 case "april":
-                    System.out.println("April is added to the Month list\n");
-                    break;
                 case "may":
-                    System.out.println("May is added to the Month list\n");
-                    break;
                 case "juine":
-                    System.out.println("June is added to the Month list\n");
-                    break;
                 case "july":
-                    System.out.println("July is added to the Month list\n");
-                    break;
                 case "august":
-                    System.out.println("August is added to the Month list\n");
-                    break;
                 case "september":
-                    System.out.println("September is added to the Month list\n");
-                    break;
                 case "october":
-                    System.out.println("October is added to the Month list\n");
-                    break;
                 case "november":
-                    System.out.println("November is added to the Month list\n");
-                    break;
                 case "december":
-                    System.out.println("December is added to the Month list\n");
+                    System.out.printf("%s is added to the Month list\n", chosenMonth);
                     break;
                 default:
                     System.out.println("Nothing is added to the Month list\n");
@@ -98,7 +58,7 @@ public class Main {
             }
 
             List<String> monthList = new ArrayList<String>();
-            monthList.add(addToMonthList.toUpperCase());
+            monthList.add(chosenMonth.toUpperCase());
 
             monthList.forEach(System.out::println);
         } catch (InputMismatchException ignore) {
